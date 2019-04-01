785378594ffae2aee7104d5cc4f48eca2af1f158
Rohit Garg <rohit.garg@quantumblack.com>
2019-04-01 16:13:51 +0530

    Added scripts and requirements

---
+++ create-branch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+if [ -z "$BRANCH" ]
+  then
+    echo "Not in version control"
+    exit 1
+fi
+
+if [ "$BRANCH" -ne "master" ]
+  then
+    echo "Create new tags from master only"
+    exit 2
+fi
+if [ -z "$1" ]
+  then
+    echo "Specify the branch to create"
+    exit 1
+fi
+
+NEEDED=$1
+echo "On branch -> $BRANCH"
+echo "Making branch -> $NEEDED"
+git branch $NEEDED && git checkout $NEEDED
+
+echo "FROM bde2020/spark-base:${NEEDED}" > Dockerfile
+cat Dockerfile.template >> Dockerfile
+
+git add Dockerfile
+git commit -m "Set base version in dockerfile"
+
+git push origin $NEEDED
